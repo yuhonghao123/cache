9b60314dfff15af3d9662f431e07f789c8e4e473
37.20℃ <2019yuhh@gmail.com>
2021-06-21 11:46:25 +0800

    update qps read and write

--- src/main/java/com/example/demo/service/impl/CacheTest.java
+++ src/main/java/com/example/demo/service/impl/CacheTest.java
@@ -6,14 +6,27 @@ import com.example.demo.po.CachePo;
 public class CacheTest {
     public static void main(String[] args) throws InterruptedException {
         CachePo<String, Integer> cache = new CachePo<>(4000);
+        long writeStartTime=System.currentTimeMillis();
         for (int i = 0; i < 100000; i++) {
             cache.put("A"+i, i);
             cache.put("B"+i, i,4000);
         }
+        long writeEndTime=System.currentTimeMillis();
+        long writeQps=100000/(writeEndTime-writeStartTime);
 //        cache.put("AAAA", 3);
 //        cache.put("BBBB", 4, 4000);
         System.out.println(cache.get("A1"));
         System.out.println(cache.get("A99999"));
+        System.out.println("writeQps:"+writeQps);
+
+        long readStartTime=System.currentTimeMillis();
+        for (int i = 0; i < 100000; i++) {
+            cache.get("A"+i);
+            cache.get("B"+i);
+        }
+        long readEndTime=System.currentTimeMillis();
+        long readQps=100000/(readEndTime-readStartTime);
+        System.out.println("readQps:"+readQps);
 
         Thread.sleep(3000);
         System.out.println(cache.get("B99"));
