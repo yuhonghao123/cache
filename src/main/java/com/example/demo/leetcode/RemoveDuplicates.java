fb3e25c7bb674e7cf525b7fa7d9564b397023416
37.20℃ <2019yuhh@gmail.com>
2021-07-04 13:10:08 +0800

    update leetcode

---
+++ src/main/java/com/example/demo/leetcode/RemoveDuplicates.java
@@ -0,0 +1,26 @@
+package com.example.demo.leetcode;
+
+/*
+    给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。
+
+不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成
+
+ */
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        int fast = 1, slow = 1;
+        while (fast < n) {
+            if (nums[fast] != nums[fast - 1]) {
+                nums[slow] = nums[fast];
+                ++slow;
+            }
+            ++fast;
+        }
+        return slow;
+
+    }
+}
---
+++ src/main/java/com/example/demo/leetcode/StrStr.java
@@ -0,0 +1,24 @@
+package com.example.demo.leetcode;
+
+/*
+    给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1
+
+ */
+public class StrStr {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length(), m = needle.length();
+        for (int i = 0; i + m <= n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < m; j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
