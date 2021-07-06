0f9c45af80c7903c5c84aa5cdb9cb8b64a2a98c7
37.20℃ <2019yuhh@gmail.com>
2021-07-06 10:15:43 +0800

    update leetcode

---
+++ src/main/java/com/example/demo/leetcode/AddBinary.java
@@ -0,0 +1,27 @@
+package com.example.demo.leetcode;
+
+/*
+    给你两个二进制字符串，返回它们的和（用二进制表示）。
+
+输入为 非空 字符串且只包含数字 1 和 0
+ */
+public class AddBinary {
+    public String addBinary(String a, String b) {
+        StringBuffer ans = new StringBuffer();
+
+        int n = Math.max(a.length(), b.length()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
+            carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
+            ans.append((char) (carry % 2 + '0'));
+            carry /= 2;
+        }
+
+        if (carry > 0) {
+            ans.append('1');
+        }
+        ans.reverse();
+
+        return ans.toString();
+    }
+}
